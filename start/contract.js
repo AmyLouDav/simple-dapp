78ab8fca2503b5cdf120ca4fd0acdeae6d9cca02
amyloudav <amyloudav@gmail.com>
2022-03-18 10:57:44 +0100

    not installed dapp flow done

--- start/contract.js
+++ start/contract.js
@@ -42,11 +42,31 @@ const initialize = () => {
     return Boolean(ethereum && ethereum.isMetaMask);
   }
 
+  //create a new metamask onboarding object to use in our app
+  const onboarding = new MetaMaskOnboarding({ forwarderOrigin })
+
+  //This will start the onboarding process
+  const onClickInstall = () => {
+    onboardButton.innerText = 'Onboarding in progress';
+    onboardButton.disabled = true;
+    //on this object we have startOnboarding which will start the onboarding process for the end user
+    onboarding.startOnboarding();
+  };
+
+  //now if the end user doesnt have the metamask extension they can install it. When they refresh the page the ethereum window object will be there and we can connect their metamask wallet to the dapp
+  
+
+
+  //this function will be called when we click a button and disable it
   const MetaMaskClientCheck = () => {
     //now check if metamask is installed
     if (!isMetaMaskInstalled()) {
       //if it isnt installed then ask user to click and install it
       onboardButton.innerText = 'Click here to install MetaMask!';
+      //when button is clicked we call this function
+      onboardButton.onclick = onClickInstall;
+      //the button is now disabled
+      onboardButton.disabled = false;
     } else {
       //if it is already installed button text changes
       onboardButton.innerText = 'Connect!'
